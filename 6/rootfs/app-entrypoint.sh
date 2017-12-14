099d987509a19f9bc0d7bc61411c873c1f07cd06
cidudp <cidudp@gmail.com>
2017-12-13 23:37:18 -0300

    Update app-entrypoint.sh
    ERROR JNDIResourceProvider,localhost-startStop-1:75 - error closing context
    javax.naming.OperationNotSupportedException: Context is read only

--- 6/rootfs/app-entrypoint.sh
+++ 6/rootfs/app-entrypoint.sh
@@ -7,6 +7,7 @@ print_welcome_page
 
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/init.sh" ]]; then
   nami_initialize tomcat jasperreports
+  echo tbeller.usejndi=false > /opt/bitnami/jasperreports/WEB-INF/classes/resfactory.properties
   info "Starting jasperreports... "
 fi
 
